374421698d885b3eb110eb51588d6ccdbcad43de
CherryPichu <uskawjdu@gmail.com>
2024-07-22 19:50:33 +0900

    git commit -m "HTTPS-37 <Agent 개발  환경 세팅>"

---
+++ Core/agent.go
@@ -0,0 +1,21 @@
+package Core
+
+/*
+*
+Agent 는 한 빌더 패턴 및 추상 팩토리 패턴으로 생성됩니다.
+*/
+type Agent struct {
+	status          string // Const 변수로 다시 변경행야함
+	hostIP          string // Const 변수로 다시 변경행야함
+	Protocol        string // Const 변수로 다시 변경행야함
+	os_name         string // ex. window
+	os_vsersion     string // ex. 10.4.5
+	os_install_date string //
+	os_tiemstamp    string //
+	UUID            string //
+
+}
+
+func (anget *Agent) init() {
+
+}
---
+++ main.go
@@ -0,0 +1,5 @@
+package Agent
+
+func main() {
+
+}
